73724883a021ff6d2d7c3dd3054eefabcfde2c5d
Naren Kirby <@narenbiardo@gmail.com>
2021-11-01 02:01:21 -0300

    se crea my-profile

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,6 +1,25 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+function loadData(email){
+    var perfil = localStorage.getItem(email.toString());
+    if(perfil){
+        var perfilParseado = JSON.parse(perfil);
+        document.getElementById("inputNombre").value = perfilParseado.nombre;
+        document.getElementById("inputEdad").value = perfilParseado.edad;
+        document.getElementById("inputEmail").value = perfilParseado.email;
+        document.getElementById("inputTelefono").value = perfilParseado.telefono;
+    }
+}
 
+function saveData() {
+    var email = document.getElementById("inputEmail").value;
+
+    localStorage.setItem(email.toString(), JSON.stringify({
+        nombre: document.getElementById("inputNombre").value,
+        edad: document.getElementById("inputEdad").value,
+        email: document.getElementById("inputEmail").value,
+        telefono: document.getElementById("inputTelefono").value
+    }));
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    loadData(localStorage.getItem("user").toString());
 });
